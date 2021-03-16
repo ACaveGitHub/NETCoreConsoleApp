27472d2485426b31b3932e172898cd017a15307d
Adam Cave <adacave_09@hotmail.com>
2021-03-16 17:37:22 -0500

    Add project files.

---
+++ GreetingService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace NETCoreConsoleApp
+{
+    partial class Program
+    {
+        public class GreetingService : IGreetingService
+        {
+            private readonly ILogger<GreetingService> _log;
+            private readonly IConfiguration _config;
+
+            public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+            {
+                _log = log;
+                _config = config;
+            }
+
+            public void Run()
+            {
+                for (int i = 0; i < _config.GetValue<int>("LoopAmount"); i++)
+                {
+                    _log.LogInformation("Run # {runNumber}", i);
+                }
+            }
+        }
+    }
+}
